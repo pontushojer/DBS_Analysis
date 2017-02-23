257fbe99f7f365e17fa21cf1295f5c0a7a164e1d
elhb <eriklhb@gmail.com>
2017-02-23 16:54:27 +0100

    rest of the files

--- archived/setup_hamming.py
+++ archived/setup_hamming.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize(["hamming_cython_solution.pyx"]))
+setup(ext_modules=cythonize(["dbs_analysis/hamming_cython_solution.pyx"]))
 
 # adapted from https://github.com/kwmsmith/scipy-2015-cython-tutorial
